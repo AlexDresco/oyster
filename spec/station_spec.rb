557c409fd87bd2baed12be2c2e7848f80bb0b10d
AlexDresco <alexandra.d1510@gmail.com>
2018-09-13 14:43:46 +0100

    Step 13 - created station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,4 +2,13 @@ require 'station'
 
 describe Station do
 
+  let(:station) {Station.new("Barbican","Zone 1")}
+
+  it "should initialize with a name" do
+    expect(station.name).to eq "Barbican"
+  end
+
+  it "should initialize with a zone" do
+    expect(station.zone).to eq("Zone 1")
+  end
 end
